0ebd95962f475f917577d99645c3c56ae49d5a01
astzls213 <astzls213@163.com>
2019-10-04 23:43:44 +0800

    hello,OpenGL.

---
+++ CExercise/helloOpenGL/cmake-build-debug/CMakeFiles/helloOpenGL.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/helloOpenGL.dir/main.cpp.o"
+  "CMakeFiles/helloOpenGL.dir/glad.c.o"
+  "helloOpenGL.pdb"
+  "helloOpenGL"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/helloOpenGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
