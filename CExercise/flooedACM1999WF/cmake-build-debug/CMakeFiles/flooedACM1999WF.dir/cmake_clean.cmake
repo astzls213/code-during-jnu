6be0976661dd4b84480cbe66cbf813f0e8049af5
astzls213 <astzls213@163.com>
2019-10-09 15:10:02 +0800

    poj: 1877

---
+++ CExercise/flooedACM1999WF/cmake-build-debug/CMakeFiles/flooedACM1999WF.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/flooedACM1999WF.dir/main.c.o"
+  "flooedACM1999WF.pdb"
+  "flooedACM1999WF"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/flooedACM1999WF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
